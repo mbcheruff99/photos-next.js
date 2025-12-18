dce16e879ab78506a86e0ca8d62284022e0d931e
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-12-17 21:19:44 -0500

    Add create API route

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -7,3 +7,20 @@ export async function GET() {
   const photos = await prisma.photo.findMany();  // Photo.all
   return NextResponse.json(photos);
 }
+
+// CREATE - POST /api/photos
+// Like: def create; @photo = Photo.create(params); end
+export async function POST(request: Request) {
+  const data = await request.json(); //like params in rails
+
+  const photo = await prisma.photo.create({
+    data: {
+      name: data.name,
+      url: data.url,
+      width:data.width,
+      height: data.height,
+    },
+  });
+
+  return NextResponse.json(photo, { status: 201 });
+}
